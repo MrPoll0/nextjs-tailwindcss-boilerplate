0b038db63ed84b71b654e7894b5056c53818f8d2
MrPoll0 <mrp0ollo@gmail.com>
2021-10-10 02:11:45 +0200

    u didnt see nothing

--- components/Layout.js
+++ components/Layout.js
@@ -8,6 +8,7 @@ export default function Layout({ children }) {
               <meta name="viewport" content="width=device-width, initial-scale=1" />
               <meta charSet="utf-8" />
 
+              <title>NextJS + TailwindCSS boilerplate</title>
               <link rel="icon" type="image/png" href="/favicon.ico" />
               <link rel="apple-touch-icon" href="/favicon.ico" />
           </Head>
